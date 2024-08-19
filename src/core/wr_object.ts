d1665a480a58222d01fcbaa13cbc1cedf05a5880
yusuke kajita <kajita1002@gmail.com>
2024-08-19 17:27:55 +0900

    first commit

---
+++ src/core/wr_object.ts
@@ -0,0 +1,16 @@
+import { WRNoun } from "./wr_noun";
+
+// すべてのもの
+export class WRObject{
+    
+}
+
+export class WRCountableObject extends WRObject{
+    
+}
+
+
+// 不可算名詞
+export class WRUncountableObject extends WRObject{
+    
+}
